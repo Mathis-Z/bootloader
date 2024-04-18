5abe07351b64879130ea5dfb9a64aa853d6e1732
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-04-18 18:18:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+https://github.com/tianocore/tianocore.github.io/wiki/Getting-Started-Writing-Simple-Application
+
+Setup with Linux and GCC:
+
+https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC
+
+https://github.com/tianocore/tianocore.github.io/wiki/Common-instructions
+
+Mit efi.sh kann man nach einem build fix die .EFI updaten (wenn man die Variablen korrekt gesetzt hat)
+
+Nicht vergessen in virtualbox in den settings unter System "EFI aktivieren" einzuschalten, sonst wird UEFI übersprungen oder so
+
+efi.sh funktioniert nicht, wenn virtualbox läuft
+
+http://kmmoore.github.io/articles/writing-a-uefi-application/
---
+++ efi.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+EFI=edk2/Build/MdeModule/DEBUG_GCC/X64/HelloWorld.efi
+VDI_IMAGE_UUID=a36011c5-bf38-42d5-8c46-a816cb5ef5ab
+EFI_VOL=vol1
+
+EFI_FILE_TO_REPLACE=vbox_efi_vol/EFI/ubuntu/grubx64.efi # eigentlich wollen wir EFI/BOOT/BOOTX64.EFI ersetzen, aber das klappt irgendwie nicht :( vielleicht mal mit nem USB stick und echter hardware ausprobieren?
+ORIGINAL_EFI_SAVE_LOCATION="$EFI_FILE_TO_REPLACE.ORIGINAL"
+
+
+mount_efi() {
+    sudo umount vbox_efi_vol 2>/dev/null
+    sudo umount vbox_sysdisk 2>/dev/null
+
+    mkdir vbox_sysdisk
+    vboximg-mount -i $VDI_IMAGE_UUID -o allow_root vbox_sysdisk --rw
+
+    if [ $? != 0 ]; then
+        echo "mounting failed, exiting..."
+        exit
+    fi
+
+    mkdir vbox_efi_vol
+    sudo mount vbox_sysdisk/$EFI_VOL vbox_efi_vol
+}
+
+umount_efi() {
+    sudo umount vbox_efi_vol 2>/dev/null
+    sudo umount vbox_sysdisk 2>/dev/null
+
+    # delete directories if empty
+    if [ -z "$(ls -A vbox_sysdisk)" ]; then
+        rm -r vbox_sysdisk
+    fi
+    if [ -z "$(ls -A vbox_efi_vol)" ]; then
+        rm -r vbox_efi_vol
+    fi
+}
+
+write_new_efi() {
+    sudo cp -n $EFI_FILE_TO_REPLACE $ORIGINAL_EFI_SAVE_LOCATION
+    sudo cp $EFI $EFI_FILE_TO_REPLACE
+}
+
+restore_old_efi() {
+    sudo cp $ORIGINAL_EFI_SAVE_LOCATION $EFI_FILE_TO_REPLACE && sudo rm $ORIGINAL_EFI_SAVE_LOCATION
+}
+
+check_if_installed() {
+    # Check the return code
+    if ls "$ORIGINAL_EFI_SAVE_LOCATION"; then
+        echo "custom EFI is installed"
+    else
+        echo "custom EFI is not installed"
+    fi
+}
+
+
+
+
+
+# Check if the number of arguments provided is not equal to 1
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [install|uninstall|installed|mount|umount]"
+    exit 1
+fi
+
+if [ $1 = "mount" ]; then
+    mount_efi
+    echo mounted
+fi
+
+if [ $1 = "umount" ]; then
+    umount_efi
+    echo unmounted
+fi
+
+
+if [ $1 = "install" ]; then
+    mount_efi
+    write_new_efi
+    umount_efi
+    echo installed
+fi
+
+if [ $1 = "uninstall" ]; then
+    mount_efi
+    restore_old_efi
+    umount_efi
+    echo uninstalled
+fi
+
+if [ $1 = "installed" ]; then
+    mount_efi
+    check_if_installed
+    umount_efi
+fi
