6cb828b05cb0085509464a6ef9c2f5b204361fec
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-04-24 20:19:00 +0200

    Makefile fix

--- myboot/qemu/arm/run.sh
+++
@@ -1,6 +0,0 @@
-cp ../../target/aarch64-unknown-uefi/debug/myboot.efi esp/efi/boot/bootx64.efi
-
-qemu-system-aarch64 -enable-kvm \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
-    -drive format=raw,file=fat:rw:esp
