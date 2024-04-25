c0ce346adb276fc6cda51c81043528063d331a69
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-04-25 13:36:44 +0200

    uefi input

--- myboot/src/main.rs
+++ myboot/src/main.rs
@@ -4,14 +4,47 @@
 
 use uefi::prelude::*;
 use uefi::println;
+use log::info;
+use uefi::proto::console::text::{Input, Key, ScanCode};
+use uefi::table::boot::BootServices;
+use uefi::{Char16, ResultExt};
+use core::prelude::v1::Ok;
+use core::fmt::Write;
+
+
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
-    uefi::helpers::init(&mut system_table).unwrap();
+fn main(_image_handle: Handle, mut st: SystemTable<Boot>) -> Status {
+    uefi::helpers::init(&mut st).unwrap();
     
     println!("Hello World!");
+
+    let mut exit_flag = false;
+
+    while !exit_flag {
+        let key = st.stdin().read_key().expect("Expected input");
+        let _ = match key {
+            Some(k) =>  {
+                match k {
+                    uefi::proto::console::text::Key::Printable(p) => {
+                        write!(st.stdout(), "A printable key was entered: {:?}\r\n", p).expect("Write failed");
+                        if p == Char16::try_from(27u16).expect("Unable to convert the ESC ascii code to Char16") {
+                            exit_flag = true;
+                        }
+                    }
+                    uefi::proto::console::text::Key::Special(s) => {
+                        write!(st.stdout(), "A special key was entered: {:?}\r\n", s).expect("Write failed");
+                        if s == ScanCode::ESCAPE {
+                            exit_flag = true;
+                        }
+                    }
+                };             
+            },
+            None => {}
+        };
+    }
     
-    system_table.boot_services().stall(3_000_000);
+    st.boot_services().stall(3_000_000);
     Status::SUCCESS
 }
 
