d93c892668d88314915a6c148054dad3a0f7efd9
yannik_lehnhardt@gmx.de <yannik_lehnhardt@gmx.de>
2024-05-12 21:30:36 +0200

    cleaning, more precise naming

--- myboot/src/main.rs
+++ myboot/src/main.rs
@@ -7,13 +7,13 @@ use uefi::table::boot::BootServices;
 use uefi::{Char16, ResultExt};
 use core::fmt::Write;
 
-mod myinput;
+mod my_list_logic;
 
 #[entry]
 fn main(_image_handle: Handle, mut st: SystemTable<Boot>) -> Status {
     uefi::helpers::init(&mut st).unwrap();
 
-    myinput::handle_input(&mut st);
+    my_list_logic::iterate_list(&mut st);
 
     st.boot_services().stall(3_000_000);
     Status::SUCCESS
