a6706c8f9ad96c7e51bbdec475c50d27dd566415
yannik_lehnhardt@gmx.de <yannik_lehnhardt@gmx.de>
2024-05-12 21:27:18 +0200

    myinput aufgeräumt, clearscreen und segment faul prevention

--- myboot/src/myinput.rs
+++ myboot/src/myinput.rs
@@ -1,6 +1,3 @@
-#![no_main]
-#![no_std]
-
 use uefi::prelude::*;
 use uefi::println;
 use uefi::table::boot::BootServices;
@@ -10,23 +7,23 @@ use core::fmt::Write;
 
 const LIST: [&str; 5] = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 
-fn print_list(st: &mut SystemTable<Boot>, selected_index: usize) {
-    
+fn print_list(st: &mut SystemTable<Boot>, mut selected_index: usize) {
+    // resets the screen
+    st.stdout().reset(false);
+
+    // goes through list and points at xth element
     for (index, item) in LIST.iter().enumerate() {
-        if index == selected_index {
+        if index == selected_index{
             write!(st.stdout(), "-> {}\r\n", item).expect("Write failed");
         } else {
             write!(st.stdout(), "   {}\r\n", item).expect("Write failed");
         }
     }
-
-    for n in 1..20 {
-        write!(st.stdout(), "\n");
-    }
 }
 
 pub fn handle_input(st: &mut SystemTable<Boot>) {
     let mut selected_index = 0;
+
     print_list(st, selected_index);
 
     let exit_flag = false;
@@ -36,12 +33,14 @@ pub fn handle_input(st: &mut SystemTable<Boot>) {
         let _ = match key {
             Some(k) => {
                 match k {
+                    // checks if pressed key is ArrowUp
                     uefi::proto::console::text::Key::Special(uefi::proto::console::text::ScanCode::UP) => {
-                        selected_index = core::cmp::max(selected_index - 1, 0);
+                        selected_index = selected_index.saturating_sub(1); // preventing segmentation fault
                         print_list(st, selected_index);
                     }
+                    // checks if pressed key is ArrowDown
                     uefi::proto::console::text::Key::Special(uefi::proto::console::text::ScanCode::DOWN) => {
-                        selected_index = core::cmp::min(selected_index + 1, LIST.len() - 1);
+                        selected_index = selected_index.saturating_add(1).min(LIST.len() - 1); // preventing segmentation fault
                         print_list(st, selected_index);
                     }
                     uefi::proto::console::text::Key::Printable(p) => {
